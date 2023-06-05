278c19b4e47f6bde0d428f660d75a90ef3cd052d
muhhuzaifa@students.uit.edu <muhhuzaifa@students.uit.edu>
2023-06-05 03:11:32 -0700

    Prepare Base Model Completed

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
 from pathlib import Path
+from dataclasses import dataclass
+from pathlib import Path
+#Entity
 
 @dataclass(frozen=True)
 class DataIngestionConfig:
@@ -7,4 +10,14 @@ class DataIngestionConfig:
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
-
+    
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
